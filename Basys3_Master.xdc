294952f147491f67b49ce5f76da9c393f0676fae
Yogesh Chhabra <chhabra99yogesh@gmail.com>
2018-11-12 16:18:26 +0530

    edited main.v

--- Basys3_Master.xdc
+++ Basys3_Master.xdc
@@ -108,10 +108,10 @@ set_property PACKAGE_PIN W4 [get_ports {an1[3]}]
 
 
 ##Buttons
-set_property PACKAGE_PIN U18 [get_ports clr1]						
+set_property PACKAGE_PIN U18 [get_ports startbtn]						
+	set_property IOSTANDARD LVCMOS33 [get_ports startbtn]
+set_property PACKAGE_PIN T18 [get_ports clr1]						
 	set_property IOSTANDARD LVCMOS33 [get_ports clr1]
-#set_property PACKAGE_PIN T18 [get_ports btnU]						
-	#set_property IOSTANDARD LVCMOS33 [get_ports btnU]
 #set_property PACKAGE_PIN W19 [get_ports btnL]						
 	#set_property IOSTANDARD LVCMOS33 [get_ports btnL]
 #set_property PACKAGE_PIN T17 [get_ports btnR]						
--- Catch_the_Light.srcs/sources_1/new/main.v
+++ Catch_the_Light.srcs/sources_1/new/main.v
@@ -22,23 +22,58 @@
 
 
 module main(
-    input clk,
+    input clk1,
     input [15:0] sw,
+    input clr1,
     input [15:0] led,
-    input [6:0] a2g,
-    input [3:0] an1
+    input [6:0] a_to_g,
+    input [3:0] an1,
+    input startbtn, //connected with middle push btn
+    input dp1
     );
+    reg gameOn;
+    reg reset;
+    reg pressed;
+    reg scoreBcd;
     reg [7:0] score;
     reg [5:0] count;
+    wire [3:0]rand;
+    
     wire turnOn;
     initial
      begin
+        pressed=0;
+        reset=0;
+        gameOn = 0;
         score = 8'b00000000;
         count = 6'b000000;
     end
-    RishabhModule m(.out());
+    binToBcd b(.B2(score),.bcdout2(scoreBcd));  //for score conversion into bcd for display
+    seg7decimal dis(.x(scoreBcd),.clk(clk1),.clr(clr1),.a2g(a_to_g),.an(an1),.dp(dp1) );    // bcd score passed to 7 segment for display
+    SapnaModule s(.random(rand),.reset(reset),.pressed(pressed));   //gives a random number the random number changes when pressed is made 1
+    RishabhModule r(.reset(reset),.out(turnOn));    // gives a signal turnOn after intervals Led is turned on when turnOn signal is on and score is also increases only at this point
     always @(*)begin
-        
+        if(startbtn==1) begin   // start button toggles the gameon register, all the processing is doneonly when gameon is 1
+            if(gameOn==1)
+                gameOn=0;
+            else
+                gameOn=1;
+        end
+    end
+    always @(*)begin
+        if(gameOn==1) begin
+            if(reset==0)    //reset is made one for one cycle after the gameOn is made 1, this resets the other modules like random number and turnOn to start the game 
+                reset=1;
+            else begin
+                reset=0;
+                if(turnOn==0) begin
+                    //idle
+                end
+                else begin
+                    //main logic here
+                end
+            end
+        end
     end
     
 
